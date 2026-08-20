e5ed865947af6701722c4bedf1e96a2ebd1402dc
Ahmad Fazri Nursamsi <85002577+AhmadFazriNursamsi@users.noreply.github.com>
2026-08-20 18:09:49 +0700

    chore(config): update apiBaseUrl to current local IP 10.0.10.48

--- mobile/lib/core/constants/app_constants.dart
+++ mobile/lib/core/constants/app_constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppConstants {
   static const String appName = 'CATU Pelayanan';
-  static const String appVersion = 'v2.5.0-build.20260814.183729';
+  static const String appVersion = 'v2.5.0-build.20260820.180906';
   static const String apiBaseUrl = 'http://10.0.10.130:3005'; // NestJS Local Server (Docker Port 3005)
   
   // Custom HSL Colors
